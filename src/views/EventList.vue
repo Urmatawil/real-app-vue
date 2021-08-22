6ccbb4ccb06bfa2bd9b55243af22609963f9344f
Rafael Ramirez <susurrus.ater@gmail.com>
2021-08-21 22:38:34 -0400

    add axios - services

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -8,6 +8,7 @@
 <script>
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
 
 export default {
   name: "EventList",
@@ -16,39 +17,15 @@ export default {
   },
   data() {
     return {
-      events: [
-        {
-          id: 123456,
-          category: "survival horror",
-          title: "Play Resident Evil 7",
-          description: "Watch me play this awesome horror game!",
-          location: "Twitch Channel",
-          date: "12/05/2021",
-          time: "22:00",
-          player: "Urmatawil",
-        },
-        {
-          id: 123457,
-          category: "survival horror",
-          title: "Play Outlast 2",
-          description: "Watch me play this scary game!",
-          location: "Twitch Channel",
-          date: "17/05/2021",
-          time: "23:00",
-          player: "Urmatawil",
-        },
-        {
-          id: 123458,
-          category: "FPS",
-          title: "Play Battlefield 2424",
-          description: "Watch me play the new game from this franchise",
-          location: "Twitch Channel",
-          date: "24/08/2021",
-          time: "16:00",
-          player: "Urmatawil",
-        },
-      ],
+      events: null,
     };
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data;
+      })
+      .catch((err) => console.log(err));
   },
 };
 </script>
